4c70ce2124d07f41c5bc6c610fe81335f3e28c97
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-25 10:06:00 -0500

    Cambios agregados de los ejemplos

--- 3_semana/projects/Curso_Angular/01_project/src/app/app.module.ts
+++ 3_semana/projects/Curso_Angular/01_project/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './herores/heroe/heroe.component';
-import { ListadoComponent } from './herores/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
