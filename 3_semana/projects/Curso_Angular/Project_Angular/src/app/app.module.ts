b66d387280fdc75c2c33543de53f2471adb36f09
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-26 22:09:29 -0500

    Curso de Angular desde Basico a Avanzado

---
+++ 3_semana/projects/Curso_Angular/Project_Angular/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
