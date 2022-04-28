c086be20c619c7089aedca206f00a90e75296449
VirginiaCastaneda <vikeyazull@gmail.com>
2022-04-27 20:06:58 -0500

    AppGif 7: folder donde estan los modulos de gifs y demas

--- 4_semana/Project_Smarty/gifsApp/src/app/gifs/gifs.module.ts
+++ 4_semana/Project_Smarty/gifsApp/src/app/gifs/gifs.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GifsPageComponent,
+    BusquedaComponent,
+    ResultadosComponent
+  ],
+  exports:[
+    GifsPageComponent
+  ],
   imports: [
     CommonModule
   ]
