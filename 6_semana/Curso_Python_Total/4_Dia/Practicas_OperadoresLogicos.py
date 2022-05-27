38cb17ef9e05cf36c6ac52511ec98363ff3e98c2
VirginiaCastaneda <vikeyazull@gmail.com>
2022-05-27 12:44:03 -0500

    Dia 4 Aprendiendo Python

---
+++ 6_semana/Curso_Python_Total/4_Dia/Practicas_OperadoresLogicos.py
@@ -0,0 +1,17 @@
+# p1
+num1 = 36
+num2 = 72/2
+num3 = 48
+mi_bool = (num1 > num2) and (num1 < num3)
+
+#p2
+num1 = 36
+num2 = 72/2
+num3 = 48
+mi_bool = (num1 > num2) or (num1 < num3)
+
+#p3
+frase = "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+palabra1 = "éxito"
+palabra2 = "tecnología"
+mi_bool = (not palabra1 in frase) and (not palabra2 in frase)
---
+++ 6_semana/Curso_Python_Total/4_Dia/Proyecto_Dia4.py
@@ -0,0 +1,57 @@
+from random import randint
+app = """
+------Juego para adivinar el número secreto!------
+"""
+nombre = input("Hola, ¿Cómo te llamas?: ")
+
+# crear numero random
+num_secreto = randint(1,100)
+
+# inicio
+inicio = f"""{app}
+Hola {nombre}, Soy Woopa!!! Y he pensado un número secreto
+entre el 1 al 100 y tienes 8 intentos para adivinar cual es!!! 
+¿Te animas?
+"""
+# el numero secreot es {num_secreto}
+print(inicio)
+numero_ingresar = int(input("Escribe el numero que crees que es el secreto: "))
+
+#loop para intentos
+intentos = 8
+
+# validaciones
+if numero_ingresar < 1 or numero_ingresar > 101:
+    res = f"El numero {numero_ingresar} que ingresaste no es valido, recurda que solo puedes ingresar numeros del 1 al 100"
+    print(res)
+elif 0 < numero_ingresar <= 100:
+    while intentos > 1:
+        if numero_ingresar < num_secreto:
+            res2 = "El numero es incorrecto, y es MENOR al numero secreto"
+            print(res2)
+            numero_ingresar = int(input("Escribe el numero que crees que es el secreto: "))
+        elif numero_ingresar > num_secreto:
+            res3 = "El numero es incorrecto, y es MAYOR al numero secreto"
+            print(res3)
+            numero_ingresar = int(input("Escribe el numero que crees que es el secreto: "))
+        else:
+            res4 = f"Felicidades el numero {numero_ingresar} es el numero que era secreto"
+            num_intentos = 8 - intentos
+            print(res4)
+            print(f"Adivinar sólo te costo {num_intentos} intentos, Eres muy buen adivinador")
+            break
+        intentos = intentos - 1
+    if intentos == 1:
+        print("Te acabaste tus intentos")
+
+
+
+
+
+
+
+
+
+
+
+
