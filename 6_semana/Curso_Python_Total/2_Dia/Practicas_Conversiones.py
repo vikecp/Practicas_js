a93fa8e7a7bb5034ae724683194040d1f68ac5a3
VirginiaCastaneda <vikeyazull@gmail.com>
2022-05-27 12:42:58 -0500

    Dia 2 Aprendiendo Python

---
+++ 6_semana/Curso_Python_Total/2_Dia/Practicas_Conversiones.py
@@ -0,0 +1,13 @@
+# P1
+num1 = 7.5
+print(type(int(num1)))
+
+#P2
+num2 = 10
+print(type(float(num2)))
+
+#P3
+num1 = "7.5"
+num2 = "10"
+
+print(float(num1) + int(num2))
---
+++ 6_semana/Curso_Python_Total/2_Dia/Practicas_Integers.py
@@ -0,0 +1,12 @@
+# P1
+num_entero = 10
+print(type(num_entero))
+# P2
+num_decimal = 10.88
+print(type(num_decimal))
+
+#P3
+num1 = 7.5
+num2 = 2.5
+print(type(num1 +num2))
+
