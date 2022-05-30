ad5934b8a0c4fe2a51423357854d672e01ab2023
VirginiaCastaneda <vikeyazull@gmail.com>
2022-05-30 15:01:05 -0500

    Dia 13 Aprendiendo Python

--- 6_semana/Curso_Python_Total/13_Dia/Asistente_voz.py
+++ 6_semana/Curso_Python_Total/13_Dia/Asistente_voz.py
@@ -9,7 +9,6 @@ import wikipedia
 
 
 # escuchar nuestro microfono y devolver el audio como texto
-
 def transformar_audio_en_texto():
 
     # almacenar recognizer en variable
@@ -60,6 +59,162 @@ def transformar_audio_en_texto():
             # devolver error
             return 'Sigo esperando'
 
+# transformar_audio_en_texto()
 
-transformar_audio_en_texto()
 
+# Voces para el texto
+en = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+es = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0'
+
+# pasar de texto a voz
+def hablar(mensaje):
+
+    # encender el moto de pyttsx3
+    engine = pyttsx3.init()
+    engine.setProperty('voice', es)
+
+    # pronunciar el msj
+    engine.say(mensaje)
+    engine.runAndWait()
+# hablar('Hola como estas?. Esta corriendo el carro')
+
+
+# informar el dia de la semana
+def pedir_dia():
+
+    # crear variable de datos de hoy
+    dia = datetime.date.today()
+
+    # crear varible para dia de la semana
+    dia_semana = dia.weekday()
+
+    # dicionario con nombres de los dias
+    calendario = {
+        0: 'Lunes',
+        1: 'Martes',
+        2: 'Miércoles',
+        3: 'Jueves',
+        4: 'Viernes',
+        5: 'Sábado',
+        6: 'Domingo'
+    }
+    dia_voz = f'El dia de hoy es {calendario[dia_semana]}'
+    # decir el dia de la semna
+    print(dia_voz)
+    hablar(dia_voz)
+
+
+# informar hora
+def pedir_hora():
+    # var con datos de la hora
+    hora = datetime.datetime.now()
+    hora = f'En este momento son las {hora.hour} horas con {hora.minute} minutos'
+    print(hora)
+
+    # decir hora
+    hablar(hora)
+
+
+
+# saludo inicial
+
+def saludo_inicial():
+    # crear variable con datos de hora
+    hora = datetime.datetime.now()
+    if hora.hour < 6 or hora.hour > 20:
+        momento = 'Buenas Noches'
+    elif 6 <= hora.hour < 13:
+        momento = 'Buen dia'
+    else:
+        momento = 'Buenas tardes'
+
+    hablar(f'Hola, {momento}. soy Michi, tu asistente personal. Por favor, dime ¿Cómo te puedo ayudar?')
+
+
+# funcion central del asistente
+def pedir_cosas():
+
+    # activar saludo inicial
+    saludo_inicial()
+
+    # variable de corte
+    comenzar = True
+
+    # loop central
+    while comenzar:
+
+        # activar el micro, y guardar en un string
+        pedido = transformar_audio_en_texto().lower()
+
+        if 'abrir youtube' in pedido:
+            hablar('Con gusto, estoy abriendo Youtube')
+            webbrowser.open('https://www.youtube.com')
+            continue
+        elif 'abrir navegador' in pedido:
+            hablar('Claro estoy en eso')
+            webbrowser.open('https://www.google.com')
+            continue
+        elif 'qué dia es hoy' in pedido:
+            pedir_dia()
+            continue
+        elif 'qué hora es' in pedido:
+            pedir_hora()
+            continue
+        elif 'busca en wikipedia' in pedido:
+            hablar("Buscando en wikipedia")
+            pedido = pedido.replace('busca en wikipedia', '')
+            wikipedia.set_lang('es')
+            resultado = wikipedia.summary(pedido, sentences=1)
+            hablar('Wikipedia dice lo siguiente')
+            hablar(resultado)
+            continue
+        elif 'busca en internet' in pedido:
+            hablar('Ya mismo estoy en eso')
+            pedido = pedido.replace('busca en internet', '')
+            pywhatkit.search(pedido)
+            hablar('Esto es lo que he encontrado')
+            continue
+        elif 'reproducir' in pedido:
+            hablar('ya comienzo a reproducirlo')
+            pywhatkit.playonyt(pedido)
+            continue
+        elif 'broma' in pedido:
+            hablar(pyjokes.get_joke('es'))
+            continue
+        elif 'precio de las acciones' in pedido:
+            accion = pedido.split('de')[-1].strip()
+            cartera = {'apple':'APPL',
+                       'amazon':'AMZN',
+                       'google:':'GOOGL'}
+            try:
+                accion_buscada = cartera[accion]
+                accion_buscada = yf.Ticker(accion_buscada)
+                precio_actual = accion_buscada.info['regularMarketPrice']
+                hablar(f'LA encontré, el precio de {accion} es {precio_actual}')
+                continue
+            except:
+                hablar('Perdón pero no la he encontrado')
+                continue
+        elif 'adiós' in pedido:
+            hablar('Me voy a descansar, Cualquier cosa me avisas')
+            break
+
+
+pedir_cosas()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Ver opciones de voz
+# engine = pyttsx3.init()
+# for voz in engine.getProperty('voices'):
+    # print(voz)
